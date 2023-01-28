814767409f916d83d9a4e4c107d99e557d3dcffd
pcrelier <pcrelier@hotmail.com>
2023-01-28 14:58:01 -0300

    Move Statistics to footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,4 +1,4 @@
-import { Box, Link, Typography } from "@mui/material";
+import { Box, Grid, Link, Typography } from "@mui/material";
 import React from "react";
 import {
   FaDiscord,
@@ -10,63 +10,103 @@ import {
 import { MdForum } from "react-icons/md";
 export function Footer() {
   return (
-    <Box
-      sx={{
-        p: 4,
-        textAlign: "center",
-        bgcolor: "#000",
-        minHeight: "20vh",
-      }}
-    >
+    <>
       <Box
+        bgcolor="background.darker"
         sx={{
-          display: "flex",
-          mt: 2,
-          mb: 6,
-          justifyContent: "center",
-          fontSize: "2rem",
-          "& *": {
-            color: "white",
-          },
-          "& *:hover": {
-            color: "primary.main",
-          },
-          "& *:not(:last-child)": {
-            mr: 6,
-          },
+          pt: 4,
+          pb: 4,
+          minHeight: "10rem",
+          maxHeight: "14rem",
         }}
       >
-        <Link href="https://discordapp.com/invite/XzqxyV7" target="_blank">
-          <FaDiscord />
-        </Link>
-        <Link href="https://twitter.com/OfficialDYOM" target="_blank">
-          <FaTwitter />
-        </Link>
-        <Link href="https://www.tiktok.com/@officialdyom" target="_blank">
-          <FaTiktok />
-        </Link>
-        <Link href="https://www.youtube.com/@dyom5505" target="_blank">
-          <FaYoutube />
-        </Link>
-        <Link
-          href="https://www.facebook.com/designyourownmission"
-          target="_blank"
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "70%",
+            m: "auto",
+          }}
         >
-          <FaFacebook />
-        </Link>
-        <Link
-          href="https://gtaforums.com/forum/263-design-your-own-mission/"
-          target="_blank"
-        >
-          <MdForum />
-        </Link>
+          <Typography variant="h3" sx={{ display: "block", width: "100%" }}>
+            Statistics
+          </Typography>
+          <Box sx={{ display: "flex", gap: "50%", "& *": { flexGrow: "1" } }}>
+            <Box>
+              <Typography variant="subtitle1">Missions: {}</Typography>
+              <Typography variant="subtitle1">Mission Packs: {}</Typography>
+              <Typography variant="subtitle1">Dump Projects: {}</Typography>
+              <Typography variant="subtitle1">Video Tutorials: {}</Typography>
+              <Typography variant="subtitle1">Text Tutorials: {}</Typography>
+            </Box>
+            <Box>
+              <Typography variant="subtitle1">Members: {}</Typography>
+              <Typography variant="subtitle1">Newest Member: {}</Typography>
+              <Typography variant="subtitle1">Members Online: {}</Typography>
+              <Typography variant="subtitle1">Guests Online: {}</Typography>
+            </Box>
+          </Box>
+        </Box>
       </Box>
-      <Link href="/report" variant="subtitle1">
-        Report an issue
-      </Link>
-      <Typography variant="subtitle1">
-        © 2008-2014 Dutchy3010 & PatrickW
-      </Typography>
-    </Box>
+
+      <Box
+        sx={{
+          p: 4,
+          textAlign: "center",
+          bgcolor: "#000",
+          minHeight: "20vh",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            mt: 2,
+            mb: 6,
+            justifyContent: "center",
+            fontSize: "2rem",
+            "& *": {
+              color: "white",
+            },
+            "& *:hover": {
+              color: "primary.main",
+            },
+            "& *:not(:last-child)": {
+              mr: 6,
+            },
+          }}
+        >
+          <Link href="https://discordapp.com/invite/XzqxyV7" target="_blank">
+            <FaDiscord />
+          </Link>
+          <Link href="https://twitter.com/OfficialDYOM" target="_blank">
+            <FaTwitter />
+          </Link>
+          <Link href="https://www.tiktok.com/@officialdyom" target="_blank">
+            <FaTiktok />
+          </Link>
+          <Link href="https://www.youtube.com/@dyom5505" target="_blank">
+            <FaYoutube />
+          </Link>
+          <Link
+            href="https://www.facebook.com/designyourownmission"
+            target="_blank"
+          >
+            <FaFacebook />
+          </Link>
+          <Link
+            href="https://gtaforums.com/forum/263-design-your-own-mission/"
+            target="_blank"
+          >
+            <MdForum />
+          </Link>
+        </Box>
+        <Link href="/report" variant="subtitle1">
+          Report an issue
+        </Link>
+        <Typography variant="subtitle1">
+          © 2008-2014 Dutchy3010 & PatrickW
+        </Typography>
+      </Box>
+    </>
   );
 }
