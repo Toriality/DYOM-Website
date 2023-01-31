45e633e59d9d0c1a904b2f3dae0788f464a91261
pcrelier <pcrelier@hotmail.com>
2023-01-31 10:43:46 -0300

    Define folder structure for uploads

--- server/upload.js
+++ server/upload.js
@@ -1,35 +1,15 @@
 const router = require("express").Router();
 const multer = require("multer");
+const fs = require("fs");
 
-// // Create storage location and filename settings
-// function createStorage(folder, filetype) {
-//   // Get current date & time numbers
-//   const currentTime = new Date().toLocaleString().replace(/\D/g, "");
-//   // Create storage
-//   const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, "uploads/" + folder);
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, currentTime + "-" + file.originalname);
-//     },
-//   });
-//   return storage;
-// }
-
-// // Create upload method
-// function createUpload(name, storage) {
-//   const upload = multer({ storage: storage }).single(name);
-//   return upload;
-// }
-
-// SET STORAGE
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    const path = `./uploads/${req.user.id}/missions/uploading`;
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 var upload = multer({ storage: storage });
