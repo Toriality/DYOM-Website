067cb65443577dc6344727b581ee35497d0fe4a4
pcrelier <pcrelier@hotmail.com>
2023-02-08 13:03:29 -0300

    scroll to top animation on route change

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,22 @@ import { AddMission } from "./components/projects/missions/addMission/AddMission
 import { ListMissions } from "./components/projects/missions/listMissions/ListMissions";
 import { FAQ } from "./components/faq/FAQ";
 import { Register } from "./components/register/Register";
+import { useLocation } from "react-router-dom";
+import React from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 // Site
 function App() {
@@ -16,6 +32,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<LayoutsWithNavbar />}>
             <Route path="/" element={<Home />} />
