65435701079ce8d7c08a003d0b1a81bc5b5fa804
pcrelier <pcrelier@hotmail.com>
2023-02-24 11:29:06 -0300

    close drawer on clicking links

--- client/src/components/navbar/Menu.js
+++ client/src/components/navbar/Menu.js
@@ -3,8 +3,20 @@ import { Grid, Typography, Link, Drawer } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 export function Menu(props) {
+  const handleLinkClick = () => {
+    props.toggle();
+  };
+  const CustomLink = (props) => {
+    return (
+      <Link component={RouterLink} to={props.to} onClick={handleLinkClick}>
+        {props.children}
+      </Link>
+    );
+  };
+
   return (
     <Drawer
+      onClose={props.toggle}
       anchor={"top"}
       open={props.open}
       ModalProps={{ onBackdropClick: props.toggle }}
@@ -14,58 +26,30 @@ export function Menu(props) {
         <Grid container>
           <Grid item xs={2.5}>
             <Typography variant="h2">General</Typography>
-            <Link component={RouterLink} to="/" variant="body1">
-              Homepage
-            </Link>
-            <Link component={RouterLink} to="news" variant="body1">
-              News
-            </Link>
-            <Link component={RouterLink} to="hall-of-fame" variant="body1">
-              Hall of Fame
-            </Link>
-            <Link component={RouterLink} to="members" variant="body1">
-              Member List
-            </Link>
-            <Link component={RouterLink} to="faq" variant="body1">
-              F.A.Q.
-            </Link>
+            <CustomLink to="/">Homepage</CustomLink>
+            <CustomLink to="/news">News</CustomLink>
+            <CustomLink to="/hall">Hall of Fame</CustomLink>
+            <CustomLink to="/members">Member List</CustomLink>
+            <CustomLink to="/faq">FAQ</CustomLink>
           </Grid>
           <Grid item xs={2.5}>
             <Typography variant="h2">Projects</Typography>
-            <Link component={RouterLink} to="missions" variant="body1">
-              Single Missions
-            </Link>
-            <Link component={RouterLink} to="mps" variant="body1">
-              Mission Packs
-            </Link>
-            <Link component={RouterLink} to="dumping-grounds" variant="body1">
-              Dumping Grounds
-            </Link>
+            <CustomLink to="/missions">Single Missions</CustomLink>
+            <CustomLink to="/mps">Mission Packs</CustomLink>
+            <CustomLink to="/dump">Dump Projects</CustomLink>
           </Grid>
           <Grid item xs={2.5}>
             <Typography variant="h2">Tutorials</Typography>
-            <Link component={RouterLink} to="video-tutorials" variant="body1">
-              Video Tutorials
-            </Link>
-            <Link component={RouterLink} to="/text-tutorials" variant="body1">
-              Text Tutorials
-            </Link>
+            <CustomLink to="/video_tutorials">Video Tutorials</CustomLink>
+            <CustomLink to="/text_tutorials">Text Tutorials</CustomLink>
           </Grid>
           <Grid item xs={4.5} align="right">
             <Typography variant="h2">User</Typography>
             <Typography variant="body1">Logged in as: {}</Typography>
-            <Link component={RouterLink} to="profile" variant="body1">
-              View Profile
-            </Link>
-            <Link component={RouterLink} to="profile/inbox" variant="body1">
-              Inbox
-            </Link>
-            <Link component={RouterLink} to="profile/settings" variant="body1">
-              Account Settings
-            </Link>
-            <Link component={RouterLink} to="profile/logout" variant="body1">
-              Log Out
-            </Link>
+            <CustomLink to="/profile">My Profile</CustomLink>
+            <CustomLink to="/profile/inbox">Inbox</CustomLink>
+            <CustomLink to="/profile/settings">Profile Settings</CustomLink>
+            <CustomLink to="/profile/logout">Logout</CustomLink>
           </Grid>
         </Grid>
       </Grid>
